da46449e90aece44715ec064c223051f4e8fa80c
jahanzaibrao-dev <jahanzaib@tetrahex.com>
2022-04-05 18:09:23 +0500

    feat: Task tracker with jason-server API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [showAddForm, setShowAddForm] = useState(false);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const taskList = await axios.get("http://localhost:5000/tasks");
+
+    setTasks(taskList.data);
+  };
+
+  const fetchTask = async (id) => {
+    const response = await axios.get(`http://localhost:5000/tasks/${id}`);
+
+    return response.data;
+  };
+
+  const deleteTask = async (id) => {
+    await axios.delete(`http://localhost:5000/tasks/${id}`);
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = async (id) => {
+    const task = await fetchTask(id);
+
+    const updatedTask = { ...task, reminder: !task.reminder };
+
+    const response = await axios.put(
+      `http://localhost:5000/tasks/${id}`,
+      updatedTask
+    );
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: response.data.reminder } : task
+      )
+    );
+  };
+
+  const addTask = async (task) => {
+    const response = await axios.post("http://localhost:5000/tasks", task);
+
+    setTasks([...tasks, response.data]);
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // setTasks([...tasks, { id, ...task }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddForm(!showAddForm)}
+          showAdd={showAddForm}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddForm && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      className="btn"
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+export default Button;
